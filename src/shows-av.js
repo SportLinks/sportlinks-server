f68cbbb1b0c74f0eb8fae64cc653a2759036f7ce
Jose Manuel Lopez Doña <jmlopezdona@gmail.com>
2016-12-29 00:42:28 +0100

    clean string

--- src/shows-av.js
+++ src/shows-av.js
@@ -30,8 +30,8 @@ export function getMainPage() {
   });
 }
 
-function cleanEvent(event) {
-  return event.replace('\n\t\t', ' ');
+function cleanString(string) {
+  return string.replace('\n\t\t', ' ');
 }
 
 export function getShows() {
@@ -44,9 +44,9 @@ export function getShows() {
       let show = {
         date: fields.eq(0).text(),
         hour: fields.eq(1).text(),
-        sport: fields.eq(2).text(),
-        competition: fields.eq(3).text(),
-        event: cleanEvent(fields.eq(4).text()),
+        sport: cleanString(fields.eq(2).text()),
+        competition: cleanString(fields.eq(3).text()),
+        event: cleanString(fields.eq(4).text()),
         channels: fields.eq(5).text(),
       }
       shows.push(show)
